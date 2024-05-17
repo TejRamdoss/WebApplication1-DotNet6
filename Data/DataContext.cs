44041dea08f080961300d313a4a58d0c50d7b764
Tej Ramdoss <ramdosstej@gmail.com>
2024-05-17 14:13:43 +0400

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<SuperHero> SuperHeroes{ get; set; }
+    }
+}
